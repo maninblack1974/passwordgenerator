055349b12f4f861390b09e81c6b781e93aa4c29c
DRH <25594179+maninblack1974@users.noreply.github.com>
2020-10-11 23:04:10 -0700

    Fomatting UI and starting javascript

--- assets/script.js
+++ assets/script.js
@@ -10,5 +10,21 @@ function writePassword() {
 
 }
 
+// prompt user for password length must be 8 to 128 characters only
+// prompt user to include lowercase
+// prompt user to include uppercase
+// prompt user to include numeric
+// prompt user to include special characters
+// validate user has at least one selected
+
+function generatePassword() {
+  var pswdLength = prompt("Enter length of desired password from 8 to 128 characters:");
+  while (pswdLength > 7 && pswdLength < 129) {
+    pswdLength = prompt("Enter length of desired password from 8 to 128 characters:");
+  }
+
+  return(pswdLength);
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
